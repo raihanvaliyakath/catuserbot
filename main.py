42a960b515729831352feadc11b4a30c4bff2982
HACKER <93648073+raihanvaliyakath@users.noreply.github.com>
2021-12-13 13:01:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+PM_START_TEXT ഹായ് ഞാൻ റൈഹാൻ TG യുടെ അസിസ്റ്റന്റ് ബോട്ട് ആണ്  ENDE SOURCE CODE [CLICK HERE] (t.me/ippokittumnokkininno)
+BOT UPDATES [M_STER_TECH] (t.me/M_STER_TECH)
+SUPPORT GROUP👨‍🏫 [M_STER_TECH_GROUP] (t.me/M_STER_TECH_GROUP)
